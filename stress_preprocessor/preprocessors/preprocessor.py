40015b0f34c3a9e452b9e98d83988fc26ce33722
Spoliti <spoliti@itml.gr>
2023-04-24 16:39:18 +0300

    float_to_integer function, preprocess baseline data, load_data_online: dict instead of array,

--- stress_preprocessor/preprocessors/preprocessor.py
+++ stress_preprocessor/preprocessors/preprocessor.py
@@ -19,16 +19,29 @@ class StressPreprocessor:
         self.config = config
         self.logger = logging.getLogger(__name__)
 
-    def load_data(self, subpaths: Dict[str, str], subj_path: str) -> List[pd.DataFrame]:
+    def float_to_integer(self, dfs):
+        formatted_types_dfs = []
+        for df in dfs:
+            df = df.astype({self.config.error_col: 'Int32', self.config.scenario_col: 'Int32',
+                            self.config.mode_col: 'Int32', self.config.participant_col: 'Int32'})
+            df.reset_index(inplace=True, drop=True)
+            formatted_types_dfs.append(df)
+        return formatted_types_dfs
+
+    def load_data(self, subpaths: Dict[str, str], baseline_subpath: str, subj_path: str, subj_id: str) -> [
+        List[pd.DataFrame],
+        List[pd.DataFrame]]:
         """
         Offline Load data from a subject's directory into a list of pandas DataFrames.
 
         Args:
             subpaths: A dictionary of file names for each scenario/mode dataset.
+            baseline_subpath: The file name for the baseline scenario.
             subj_path: The path to the directory containing the subject's raw data files.
+            subj_id: The participant's unique identification.
 
         Returns:
-            A list of pandas DataFrames, one for each scenario/mode dataset.
+            The baseline dataframe and a list of pandas DataFrames, one for each scenario/mode dataset.
         """
         start = time.time()
 
@@ -38,8 +51,37 @@ class StressPreprocessor:
             logging.error(f"Directory not found: {subj_path}")
             raise FileNotFoundError(f"Directory not found: {subj_path}")
 
+        baseline_df_list = []
+        if baseline_subpath:
+            baseline_subpath = baseline_subpath.replace("SUBJ_XX", f"SUBJ_{subj_id}")
+            filepath = os.path.join(subj_path, baseline_subpath)
+            # Check if the file exists
+            if not os.path.exists(filepath):
+                logging.error(f"Baseline data not found: {filepath}")
+                raise FileNotFoundError(f"Baseline data not found: {filepath}")
+
+            try:
+                baseline_df = pd.read_csv(filepath, header=1)
+                baseline_df = baseline_df.drop(0)
+                baseline_df = baseline_df[[self.config.time_col, self.config.ecg_col, self.config.gsr_col,
+                                           self.config.target_col, self.config.error_col, self.config.scenario_col,
+                         self.config.mode_col, self.config.participant_col]]
+                baseline_df = baseline_df.astype({self.config.time_col: 'float32', self.config.ecg_col: 'float32',
+                                                  self.config.gsr_col: 'float32', self.config.target_col: 'float32',
+                                                  self.config.error_col: 'float32', self.config.scenario_col: 'float32',
+                                                  self.config.mode_col: 'float32', self.config.participant_col: 'float32'})
+                baseline_df.reset_index(inplace=True, drop=True)
+                baseline_df_list = [baseline_df]
+                baseline_df_list = self.float_to_integer(baseline_df_list)
+                logging.info(f"Successfully loaded {filepath}")
+
+            except Exception as e:
+                logging.error(f"Error loading file: {filepath}\n{str(e)}")
+                raise ValueError(f"Error loading file: {filepath}\n{str(e)}")
+
         dfs = []
         for key, filename in subpaths.items():
+            filename = filename.replace("SUBJ_XX", f"SUBJ_{subj_id}")
             filepath = os.path.join(subj_path, filename)
 
             # Check if the file exists
@@ -48,13 +90,19 @@ class StressPreprocessor:
                 continue
 
             try:
-                df = pd.read_csv(filepath, header=1, sep=';')
+                df = pd.read_csv(filepath, header=1)
                 df = df.drop(0)
+                df = df[[self.config.time_col, self.config.ecg_col, self.config.gsr_col,
+                         self.config.target_col, self.config.error_col, self.config.scenario_col,
+                         self.config.mode_col, self.config.participant_col]]
                 df = df.astype({self.config.time_col: 'float32', self.config.ecg_col: 'float32',
                                 self.config.gsr_col: 'float32', self.config.target_col: 'float32',
-                                self.config.error_col: 'float32'})
+                                self.config.error_col: 'float32', self.config.scenario_col: 'float32',
+                                self.config.mode_col: 'float32', self.config.participant_col: 'float32'})
                 df.reset_index(inplace=True, drop=True)
+
                 dfs.append(df)
+                dfs = self.float_to_integer(dfs)
                 logging.info(f"Successfully loaded {filepath}")
 
             except Exception as e:
@@ -64,26 +112,57 @@ class StressPreprocessor:
         stop = time.time()
         logging.info(f"Data loading latency (secs): {stop - start}")
 
-        return dfs
+        return baseline_df_list, dfs
 
-    def load_data_online(self, array):
-        # Array to df
-        df = pd.DataFrame(array, columns=self.config.online.array_schema)
+    def load_data_online(self, window):
+        # Dictionary to dataframe
+        df = pd.DataFrame(window)
+
+        df = df[
+            [
+                self.config.time_col,
+                self.config.ecg_col,
+                self.config.gsr_col,
+                self.config.target_col,
+                self.config.error_col,
+                self.config.scenario_col,
+                self.config.mode_col,
+                self.config.participant_col,
+            ]
+        ]
         df = df.astype({self.config.time_col: 'float32', self.config.ecg_col: 'float32', self.config.gsr_col: 'float32',
-                        self.config.target_col: 'float32', self.config.error_col: 'float32'})
+                        self.config.target_col: 'float32', self.config.error_col: 'float32',
+                        self.config.scenario_col: 'float32', self.config.mode_col: 'float32',
+                        self.config.participant_col: 'float32'})
         return [df]
 
-    def clean_and_validate(self, dfs: List[pd.DataFrame]) -> List[pd.DataFrame]:
+    def clean_and_validate(self, baseline_df_list, dfs: List[pd.DataFrame]) -> [List[pd.DataFrame], List[pd.DataFrame]]:
         """
         Preprocesses a list of dataframes.
 
         Args:
+            baseline_df_list: List containing the baseline dataframe.
             dfs: A list of pandas dataframes to preprocess.
 
         Returns:
             A list of preprocessed pandas dataframes.
         """
         start = time.time()
+        imputed_baseline_df_list = []
+        if baseline_df_list:
+            # Remove duplicate rows from each dataframe in the input list.
+            no_dup_baseline_df_list = clean_duplicates(baseline_df_list)
+            # Validate the remaining timestamps to ensure that they are uniformly spaced.
+            val_baseline_df_list = validate_timestamps(no_dup_baseline_df_list, self.config.time_col,
+                                                       self.config.sampling_rate_hz)
+
+            # Impute missing values: bfill and ffil for categorical, interpolation for numerical (neurokit default is ffill)
+            # Before imputation, null values are added in error marked rows
+            imputed_baseline_df_list = impute_null(val_baseline_df_list, self.config.error_col, self.config.ecg_col,
+                                                   self.config.gsr_col,
+                                                   self.config.target_col, self.config.scenario_col,
+                                                   self.config.mode_col,
+                                                   self.config.participant_col)
         # Remove duplicate rows from each dataframe in the input list.
         no_dup_dfs = clean_duplicates(dfs)
         # Validate the remaining timestamps to ensure that they are uniformly spaced.
@@ -98,7 +177,7 @@ class StressPreprocessor:
         stop = time.time()
         logging.info(f"Data cleaning and timestamp validation latency (secs): {stop - start}")
 
-        return imputed_dfs
+        return imputed_baseline_df_list, imputed_dfs
 
     def visualize(self, dfs: List[pd.DataFrame]):
         # Plot ECG raw
@@ -153,13 +232,15 @@ class StressPreprocessor:
         logging.info(f"Feature extraction latency (secs): {stop - start}")
         return new_feats_dfs
 
-    def save_preprocessed_data(self, dfs: List[pd.DataFrame], subj_id: str) -> None:
+    def save_preprocessed_data(self, baseline_df_list: List[pd.DataFrame], dfs: List[pd.DataFrame],
+                               subj_id: str) -> None:
         """
         Save the preprocessed data to the "processed_data_path" defined in config. If "save_single_df" is True,
         all scenarios/modes will be saved in a single file, with time counter reset to 0 for each scenario/mode.
 
         Args:
             dfs: A list of pandas DataFrames containing the preprocessed data to be saved.
+            baseline_df_list: List containing the preprocessed baseline Dataframe to be saved.
             subj_id: A string representing the subject ID for which the data is being saved.
 
         Returns:
@@ -170,6 +251,11 @@ class StressPreprocessor:
             os.makedirs(subj_dir)
 
         filename = None
+
+        if baseline_df_list:
+            filename = f"SUBJ_{subj_id}_SCEN_00_MODE_FreeDriving.csv"
+            baseline_df = baseline_df_list[0]
+            baseline_df.to_csv(os.path.join(subj_dir, filename), index=False)
 
         if self.config.save_single_df:
             final_df = pd.concat(dfs)
@@ -184,30 +270,34 @@ class StressPreprocessor:
 
         logging.info(f"Preprocessed data saved at {os.path.join(subj_dir, filename)}")
 
-    def run(self, subpaths: Dict[str, str], subject_path: str, subj_id: str) -> None:
+    def run(self, subpaths: Dict[str, str], baseline_subpath: str, subject_path: str, subj_id: str) -> None:
         """
         Offline Preprocessing pipeline: data loading, cleaning and validation, feature extraction, store preprocessed.
 
         Args:
             subpaths: A dictionary of file names for each scenario/mode dataset.
             subject_path: The path to the directory containing the subject's raw data files.
+            baseline_subpath: The file name for the baseline scenario.
             subj_id: The participant's unique identification
 
         Returns:
             None
         """
         self.subj_id = subj_id
-        dfs = self.load_data(subpaths, subject_path)
+        baseline_df_list, dfs = self.load_data(subpaths, baseline_subpath, subject_path, subj_id)
 
         self.visualize(dfs)
-        prep_dfs = self.clean_and_validate(dfs)
+        prep_baseline_df_list, prep_dfs = self.clean_and_validate(baseline_df_list, dfs)
         new_feats_dfs = self.extract_features(prep_dfs)
-        self.save_preprocessed_data(new_feats_dfs, subj_id)
+        new_feats_baseline_df_list = self.extract_features(baseline_df_list)
 
-    def online_run(self, array: np.ndarray) -> pd.DataFrame:
+        self.save_preprocessed_data(new_feats_baseline_df_list, new_feats_dfs, subj_id)
+
+    def online_run(self, array: np.ndarray, subj_id) -> pd.DataFrame:
         start = time.time()
         dfs = self.load_data_online(array)
-        prep_dfs = self.clean_and_validate(dfs)
+        _, prep_dfs = self.clean_and_validate(None, dfs)
+        prep_dfs = self.float_to_integer(prep_dfs)
         new_feats_dfs = self.extract_features(prep_dfs, offline=False)  # Using offline=False to indicate this is online
         stop = time.time()
         logging.info(f"Overall latency (secs): {stop - start}")
